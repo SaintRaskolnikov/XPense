625b06a67c08f2e2cdc693369118f923d728e815
SaintRaskolnikov <63456965+SaintRaskolnikov@users.noreply.github.com>
2025-01-01 23:13:17 +0000

    Update urls.py to empty login

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
